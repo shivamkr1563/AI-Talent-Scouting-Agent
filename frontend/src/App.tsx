477561a326e7b3976ab67ded379f50e9c8b54e75
Shivam Kumar <shivamkr1562@gmail.com>
2026-04-26 16:38:05 +0530

    feat: did some minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import   { useState } from 'react'
 import { runAgent, AgentResult } from './api/agent'
 import { JDInput } from './components/JDInput'
 import { PhaseLog } from './components/PhaseLog'
